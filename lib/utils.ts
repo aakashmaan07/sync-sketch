29022ae3a512d341f4c7da211caf953890bcbe1e
aakashmaan07 <aakashmaan07@gmail.com>
2024-11-05 21:04:52 +0530

    Room Sidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = ["#DB2777","#059669", "#D97706", "#DC2626","#7C3AED"];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
